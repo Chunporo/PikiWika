2d55e582ab9846f614d47e376646a5c6ba1fde6a
Quoc Trung Huynh <62810853+Chunporo@users.noreply.github.com>
2024-03-19 14:13:34 +0700

    add more API

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ app.get("/pokemon", async function (req, res) {
     var pool = await conn;
     var sql_query = req.query;
     // if sql_query.id is not Null
-    console.log(sql_query);
     var sqlString = "SELECT * FROM Pokemon";
     return await pool.request().query(sqlString, function (err, data) {
         res.send(data.recordset)
@@ -39,11 +38,42 @@ app.get("/pokemon/:id", async function (req, res) {
     });
 });
 
-app.get("/pokemon/:name", async function (req, res) {
+app.get("/pokemon/name/:pokename", async function (req, res) {
+    var name = req.params.pokename;
+    var pool = await conn;
+    console.log(name)
+    var sqlString = "SELECT * FROM pokemon where pname = @varName";
+    return await pool.request().input('varName', sql.NVarChar, name).query(sqlString, function (err, data) {
+        
+        if (data.recordset.length > 0) {
+            res.send(data.recordset);
+        } else {
+            res.send(null);
+        }
+    });
+});
+
+app.get("/pokemon/move/name/:name", async function (req, res) {
     var name = req.params.name;
     var pool = await conn;
-    var sqlString = "SELECT * FROM Pokemon where pname = @varName";
-    return await pool.request().input('varName', sql.NVarCharVarChar, name).query(sqlString, function (err, data) {
+    console.log(name)
+    var sqlString = "SELECT * FROM Pokemon_Move_Name(@varName)";
+    return await pool.request().input('varName', sql.NVarChar, name).query(sqlString, function (err, data) {
+        
+        if (data.recordset.length > 0) {
+            res.send(data.recordset);
+        } else {
+            res.send(null);
+        }
+    });
+});
+
+
+app.get("/pokemon/move/id/:id", async function (req, res) {
+    var id = req.params.id;
+    var pool = await conn;
+    var sqlString = "SELECT * FROM Pokemon_Move_Id(@varId)";
+    return await pool.request().input('varId', sql.Int, id).query(sqlString, function (err, data) {
         
         if (data.recordset.length > 0) {
             res.send(data.recordset);
@@ -76,9 +106,10 @@ app.get("/move/:id", async function (req, res) {
     });
 });
 
-app.get("/move/:name", async function (req, res) {
-    var name = req.params.name;
+app.get("/move/name/:movename", async function (req, res) {
+    var name = req.params.movename;
     var pool = await conn;
+    console.log(name)
     var sqlString = "SELECT * FROM moves where mname = @varName";
     return await pool.request().input('varName', sql.NVarChar, name).query(sqlString, function (err, data) {
         
@@ -89,6 +120,82 @@ app.get("/move/:name", async function (req, res) {
         }
     });
 });
+
+app.get("/type", async function (req, res) {
+    var pool = await conn;
+    var sqlString = "SELECT * FROM types";
+    return await pool.request().query(sqlString, function (err, data) {
+        res.send(data.recordset)
+    });
+});
+
+app.get("/type/:id", async function (req, res) {
+    var id = req.params.id;
+    var pool = await conn;
+    var sqlString = "SELECT * FROM types where typeid = @varId";
+    return await pool.request().input('varId', sql.Int, id).query(sqlString, function (err, data) {
+        
+        if (data.recordset.length > 0) {
+            res.send(data.recordset);
+        } else {
+            res.send(null);
+        }
+    });
+});
+
+app.get("/type/name/:typename", async function (req, res) {
+    var name = req.params.typename;
+    var pool = await conn;
+    console.log(name)
+    var sqlString = "SELECT * FROM types where tname = @varName";
+    return await pool.request().input('varName', sql.NVarChar, name).query(sqlString, function (err, data) {
+        
+        if (data.recordset.length > 0) {
+            res.send(data.recordset);
+        } else {
+            res.send(null);
+        }
+    });
+});
+
+app.get("/ability", async function (req, res) {
+    var pool = await conn;
+    var sqlString = "SELECT * FROM abilities";
+    return await pool.request().query(sqlString, function (err, data) {
+        res.send(data.recordset)
+    });
+});
+
+app.get("/ability/:id", async function (req, res) {
+    var id = req.params.id;
+    var pool = await conn;
+    var sqlString = "SELECT * FROM abilities where abilityid = @varId";
+    return await pool.request().input('varId', sql.Int, id).query(sqlString, function (err, data) {
+        
+        if (data.recordset.length > 0) {
+            res.send(data.recordset);
+        } else {
+            res.send(null);
+        }
+    });
+});
+
+app.get("/ability/name/:abilityname", async function (req, res) {
+    var name = req.params.abilityname;
+    var pool = await conn;
+    console.log(name)
+    var sqlString = "SELECT * FROM abilities where aname = @varName";
+    return await pool.request().input('varName', sql.NVarChar, name).query(sqlString, function (err, data) {
+        
+        if (data.recordset.length > 0) {
+            res.send(data.recordset);
+        } else {
+            res.send(null);
+        }
+    });
+});
+
+
 // app.put("/update", function (req, res) {
 //     res.send("Hello")
 // });
@@ -98,6 +205,7 @@ app.get("/move/:name", async function (req, res) {
 // });
 
 
+const PORT = process.env.PORT || 3000;
 //Open host 
-app.listen(3000, function () {
+app.listen(PORT, function () {
 });
